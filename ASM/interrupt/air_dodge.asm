483394e76e537a79ba705aab04e1b8f28dde9752
RockAndRoll805 <jkentsteel@gmail.com>
2021-04-28 13:02:42 -0700

    reduced amount of instructions

--- ASM/interrupt/air_dodge.asm
+++ ASM/interrupt/air_dodge.asm
@@ -1,20 +1,10 @@
 # inject at 0x80099a70
 
-li r16, 0x20
+li r16, 0x60
 mfspr r19, 8
-bl <queue_search>
-mtspr 8, r19
-cmpwi r17, 0
-bne 0x20
-
-li r16, 0x40
-mfspr r19, 8
-bl <queue_search>
+bl <queue_search_any>
 mtspr 8, r19
 cmpwi r17, 0
 bne 0x8
 b 0x80099a88	# no airdodge
-mfspr r19, 8
-bl <queue_clear>
-mtspr 8, r19
 b 0x80099a74	# do airdodge
