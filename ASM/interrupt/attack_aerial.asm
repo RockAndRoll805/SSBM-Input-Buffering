d3484f80737baf28b2c481bbf5ef8d76ba26fd78
RockAndRoll805 <jkentsteel@gmail.com>
2021-04-28 21:44:08 -0700

    fixed a crash jabbing after aerials
    changed from static overwrite to injections to fix aforementioned crash

--- ASM/interrupt/attack_aerial.asm
+++ ASM/interrupt/attack_aerial.asm
@@ -1,9 +1,14 @@
-# overwrite at 0x8008cd88
+# WAS overwrite at 0x8008cd88
+#  now injection at 0x8008cda0
 
 lis r16, 0xF0
 ori r16, r16, 0x100
 mfspr r19, 8
-bl <queue_search>
+bl <queue_search_any>
 mtspr 8, r19
 cmpwi r17, 0
-beq 0x50
+
+
+# beq 0x8
+# b 0x8008cda4			# perform aerial
+# b 0x8008cdf0			# no aerial
--- ASM/interrupt/jab.asm
+++ ASM/interrupt/jab.asm
@@ -1,7 +1,11 @@
-# overwrite at 0x8008aa08
+# WAS overwrite at 0x8008aa08
+#  now injection at 0x8008aa1c
 
 li r16, 0x100
 mfspr r19, 8
 bl <queue_search_exclude_stick>
 mtspr 8, r19
 cmpwi r17, 0
+beq 0x8
+b 0x8008aa20			# perform jab
+b 0x8008ab18			# no jab
