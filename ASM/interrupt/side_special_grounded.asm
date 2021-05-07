d8b04c9c3c2c71151c6c8abd5fc02ed3ee6dacd8
RockAndRoll805 <jkentsteel@gmail.com>
2021-05-07 00:52:29 -0700

    fixed error in logic checking joystick values

--- ASM/interrupt/side_special_grounded.asm
+++ ASM/interrupt/side_special_grounded.asm
@@ -1,9 +1,18 @@
-# static overwrite at 0x80096574
+# WAS static overwrite at 0x80096574
+#  now injection at 0x80096578
 
-lis r16, 0xc
+lis r16, 0x4
 ori r16, r16, 0x200
 mfspr r19, 8
 bl <queue_search>
 mtspr 8, r19
 cmpwi r17, 0
-beq 0x38
+bne 0x1c
+xoris r16, r16, 0xC
+mfspr r19, 8
+bl <queue_search>
+mtspr 8, r19
+cmpwi r17, 0
+beq 0x8
+b 0x80096590
+b 0x80096584
